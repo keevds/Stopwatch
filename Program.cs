eaa2965c886bbfdf4acb07a2d4bad5b65c1231dc
Kevin Barbosa <kevinbbst@gmail.com>
2024-09-21 16:51:55 -0300

    Stopwatch finalizado-2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿static void Menu(){
+﻿Menu();
+
+static void Menu()
+{
     Console.Clear();
     Console.WriteLine("S = Segundo => 10s = 10 segundos");
     Console.WriteLine("M = Minuto => 1m = 1 minuto");
@@ -6,23 +9,51 @@
     Console.WriteLine("Quanto tempo deseja contar?");
 
     string data = Console.ReadLine().ToLower();
-    Console.WriteLine(data);
+    char type = char.Parse(data.Substring(data.Length - 1, 1));
+    int time = int.Parse(data.Substring(0, data.Length - 1));
+
+    int multiplier = 1;
+
+    if (type == 'm')
+    {
+        multiplier = 60;
+    }
+    if (time == 0)
+    {
+        System.Environment.Exit(0);
+    }
+
+    PreStart(time * multiplier);
 }
-// static void Start(int time){
-//     int currenTime = 0;
 
-//     while(currenTime != 20){
-//         Console.Clear();
-//         currenTime++;
-//         Console.WriteLine(currenTime);
-//         Thread.Sleep(1000);
-//     }
-//     Console.Clear();
-//     Console.WriteLine("Stopwatch finalizado");
-//     Thread.Sleep(2500);
-    
+static void PreStart(int time)
+{
+    Console.Clear();
+    Console.WriteLine("Ready...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Set...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Go...");
+    Thread.Sleep(2500);
 
-// }
+    Start(time);
+}
 
-// Start(20);
-Menu();
+static void Start(int time)
+{
+    int currenTime = 0;
+
+    while (currenTime != time)
+    {
+        Console.Clear();
+        currenTime++;
+        Console.WriteLine(currenTime);
+        Thread.Sleep(1000);
+    }
+    Console.Clear();
+    Console.WriteLine("Stopwatch finalizado");
+    Thread.Sleep(2500);
+
+    Menu();
+
+}
